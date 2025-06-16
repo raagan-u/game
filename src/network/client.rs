8456a1af1ca1f21cdcce9589d8b89441c5e700f5
raagan-u <n0r313n7@protonmail.ch>
2025-06-17 05:22:12 +0530

    add server to NetworkManager

---
+++ src/network/client.rs
@@ -0,0 +1,7 @@
+pub struct Client {}
+
+impl Client {
+    pub async fn new() -> Self {
+        Client {}
+    }
+}
--- src/network/network_manager.rs
+++ src/network/network_manager.rs
@@ -1,3 +1,5 @@
+use crate::network::server::Server;
+
 pub enum ManagerMode {
     Server,
     Client,
@@ -11,6 +13,17 @@ impl NetworkManager {
     pub fn builder() -> NetworkManagerBuilder {
         NetworkManagerBuilder::new()
     }
+    
+    pub async fn run(&self) {
+        match self.mode {
+            ManagerMode::Server => {
+                Server::new().run().await;
+            }
+            ManagerMode::Client => {
+                println!("Running client");
+            }
+        }
+    }
 }
 
 pub struct NetworkManagerBuilder {
@@ -19,19 +32,31 @@ pub struct NetworkManagerBuilder {
 
 impl NetworkManagerBuilder {
     pub fn new() -> Self {
-        unimplemented!();
         Self { mode: ManagerMode::Server }
     }
     
-    pub fn enable_server(&mut self) {
+    pub fn enable_server(mut self) -> Self {
         self.mode = ManagerMode::Server;
+        self
     }
     
-    pub fn enable_client(&mut self) {
+    pub fn enable_client(mut self) -> Self {
         self.mode = ManagerMode::Client;
+        self
     }
     
     pub fn build(self) -> NetworkManager {
         NetworkManager { mode: self.mode }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_server_run() {
+        let manager = NetworkManager::builder().enable_server().build();
+        manager.run().await;
     }
 }
